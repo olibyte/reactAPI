3bb591953fcd4829acc6bb38f4b586adda71d65a
Oliver <ocben1@gmail.com>
2019-10-16 19:42:47 +1100

    update news component

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import NewSingle from './NewSingle';
 
 class News extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            news: [],
+        };
+    }
+
+    componentDidMount() {
+        const url = "https://newsapi.org/v2/everything?q=bitcoin&from=2019-09-16&sortBy=publishedAt&apiKey=a7e8e064a2854146b91a2ff462757841"
+    }
     renderItems() {
         return this.props.items.map((item ) => (
             <NewSingle key={item.id} item={item} />
