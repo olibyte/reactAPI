39ed9505a7cd787c7ed05c3d3a77f0e7aa220431
Oliver <ocben1@gmail.com>
2019-10-18 08:13:59 +1100

    refactor reusable component

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,39 +2,41 @@ import React, { Component } from 'react';
 import NewSingle from './NewSingle';
 
 class News extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            news: [],
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+    };
+  }
 
-    componentDidMount() {
-        const url = `https://newsapi.org/v2/${this.props.news.type}?sources=${this.props.news.query}&apiKey=a7e8e064a2854146b91a2ff462757841`;
-        fetch(url)
-        .then((response) => {
-            return response.json();
+  componentDidMount() {
+    const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=a7e8e064a2854146b91a2ff462757841`;
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          news: data.articles
         })
-        .then((data) => {
-            this.setState({
-                news: data.articles
-            })
-        })
-        .catch((error) => console.log(error));
-    }
-    renderItems() {
-        return this.state.news.map((item) => (
-            <NewSingle key={item.url} item={item} />
-        ));
-    }
-    render() {
-        return (
-            <div className="row">
-                {this.renderItems()}
-            </div>
-            
-        );
-    }
+      })
+      .catch((error) => console.log(error));
+  }
+
+  renderItems() {
+    return this.state.news.map((item) => (
+      <NewSingle key={item.url} item={item} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="row">
+        {this.renderItems()}
+      </div>
+    );
+  }
 }
 
 export default News;
